16aac728b93fed5ef0c6d926225e84e0c3093f8a
Arthur-Louis <tutur910@gmail.com>
2023-03-10 16:26:09 +0100

    feat: add Module Placement Ascendant

---
+++ src/org/fog/placement/ModulePlacementASC.java
@@ -0,0 +1,64 @@
+package org.fog.placement;
+
+import org.fog.application.AppModule;
+import org.fog.application.Application;
+import org.fog.entities.FogDevice;
+
+import java.util.*;
+
+public class ModulePlacementASC extends ModulePlacement{
+
+	private ModuleMapping moduleMapping;
+
+	@Override
+	protected void mapModules() {
+		Map<String, List<String>> mapping = moduleMapping.getModuleMapping();
+		List<FogDevice> fogDevices = getFogDevices();
+		// Sort the fog devices in ascending order of their available MIPS
+		fogDevices.sort(Comparator.comparingDouble(FogDevice::getRatePerMips));
+
+		// Sort the service instances in ascending order of their processing requirements (MI)
+		Application app = getApplication();
+		List<AppModule> appModules = app.getModules();
+		appModules.sort(Comparator.comparingDouble(AppModule::getMips));
+		// Place the service instances one by one in the fog nodes in sorted order
+		for (AppModule appModule : appModules) {
+			FogDevice selectedDevice = null;
+			for (FogDevice fogDevice : fogDevices) {
+				if (fogDevice.getRatePerMips() >= appModule.getMips()) {
+					selectedDevice = fogDevice;
+					break;
+				}
+			}
+			if (selectedDevice != null) {
+				moduleMapping.addModuleToDevice(appModule.getName(), selectedDevice.getName());
+			} else {
+				// No suitable fog node found for the service instance
+				System.out.println("No suitable fog node found for appModule instance: " + appModule.getName());
+			}
+		}
+	}
+
+	public ModulePlacementASC(List<FogDevice> fogDevices, Application application,
+                              ModuleMapping moduleMapping){
+		this.setFogDevices(fogDevices);
+		this.setApplication(application);
+		this.setModuleMapping(moduleMapping);
+		this.setModuleToDeviceMap(new HashMap<String, List<Integer>>());
+		this.setDeviceToModuleMap(new HashMap<Integer, List<AppModule>>());
+		this.setModuleInstanceCountMap(new HashMap<Integer, Map<String, Integer>>());
+		for(FogDevice device : getFogDevices())
+			getModuleInstanceCountMap().put(device.getId(), new HashMap<String, Integer>());
+		mapModules();
+	}
+	
+	
+	public ModuleMapping getModuleMapping() {
+		return moduleMapping;
+	}
+	public void setModuleMapping(ModuleMapping moduleMapping) {
+		this.moduleMapping = moduleMapping;
+	}
+
+	
+}
